c0c75e8efa8d0e14e139c592ce266bd60eb98746
hecter_wu <hecter_wu@transcend.com.tw>
2017-05-02 17:21:33 +0800

    using Google unittest framework to write c++ unit test

---
+++ Reverse_Words_in_a_String.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <algorithm>
+#include "gtest/gtest.h"
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string &s) {
+        reverse(s.begin(),s.end());
+        int startPos = 0;
+        int length = 0;
+        int realPos = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == ' '){
+                if(length){
+                    reverse(s.begin()+startPos,s.begin()+startPos+length);
+                    length = 0;
+                    s[realPos++] = s[i];
+                }
+            }
+            else{
+                if(length == 0)
+                    startPos = realPos;
+                s[realPos++] = s[i];
+                length++;
+            }
+        }
+        if(length){
+            reverse(s.begin()+startPos,s.begin()+startPos+length);
+        }
+        s.erase(realPos);
+        s.erase(s.find_last_not_of(" ") + 1);
+		return s;
+    }
+};
+
+TEST(reverseWords, longstring) {
+  string s ="the sky is blue";
+  string ans = "blue is sky the";
+  EXPECT_EQ(Solution().reverseWords(s),ans);
+
+}
+
+int main(int argc, char** argv) {
+  // Prints elapsed time by default.
+  //testing::GTEST_FLAG(print_time) = true;
+
+  // This allows the user to override the flag on the command line.
+  testing::InitGoogleTest(&argc, argv);
+  
+  RUN_ALL_TESTS();
+  system("pause");
+  return 0;
+}
